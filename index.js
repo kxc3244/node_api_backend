3b7803c52cc3bb4487daf3e584ff5b76b684b5e1
Kevin Chawla <kevin@Kevins-MacBook-Pro.local>
2021-11-16 16:56:38 -0600

    finished code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-exports.publicAPI = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    res.status(200).send(message);
+const axios = require("axios")
+
+exports.publicAPI = async (req, res) => {
+    const response = await axios.get("https://api.publicapis.org/entries?category=animals")
+    res.status(200).send(response.data);
   };
